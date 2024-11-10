33b6e315e0629b2ba09549d79da39a0746b63ea1
borgearthur <tubodish@gmail.com>
2024-11-10 00:35:01 -0300

    AvaliaProduto

--- projeto_fds/cypress/e2e/AvaliarProduto.cy.js
+++ projeto_fds/cypress/e2e/AvaliarProduto.cy.js
@@ -1,13 +1,51 @@
 describe('test suite avaliar produtos', () => {
-    it('cenario1', () => {
-        //steps do cenario1
-    })
+    it('cenario1', () => {// valiação registrada com sucesso e visível para outros clientes.
 
-    it('cenario2', () => {
-        //steps do cenario2
-    })
+        cy.visit('/');
+        
+     cy.get('.dropdown > button').then(($button) => {
+            cy.wrap($button).trigger('mouseover');
+    
+            cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+    
+            cy.get('.dropdown-menu').should('be.visible');
+    
+            cy.get('.dropdown-menu').click(); 
+        });
 
-    it('cenario3', () => {
-        //steps do cenario3
+        cy.get('#nome_usuario').type('Mariolf');
+        cy.get('#senha').type('mar12345');
+        cy.get('form > button').click();
+
+        cy.visit('/historico_compras')
+        cy.get('[type="number"]').type('4')
+        cy.get('form > button').click()
+        cy.get('.btn').click()
+        cy.url().should('not.include', '/historico_compras');
+
+        })
+
+    it('cenario2', () => {// Exibir mensagem de erro indicando nota inválida e impedir o envio da avaliação.
+    cy.visit('/');
+        
+     cy.get('.dropdown > button').then(($button) => {
+            cy.wrap($button).trigger('mouseover');
+    
+            cy.get('.dropdown-menu').invoke('css', 'display', 'block');
+    
+            cy.get('.dropdown-menu').should('be.visible');
+    
+            cy.get('.dropdown-menu').click(); 
+        });
+
+        cy.get('#nome_usuario').type('Mariolf');
+        cy.get('#senha').type('mar12345');
+        cy.get('form > button').click();
+
+        cy.visit('/historico_compras')
+        cy.get('[type="number"]').type('11')
+        cy.get('form > button').click()
+        cy.url().should('include', '/historico_compras');
+
+        })    
     })
-})
